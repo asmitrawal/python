f82fe5b2659541f235837e09436eb9e79d1ead18
asmitrawal <107305159+asmitrawal@users.noreply.github.com>
2022-07-05 17:01:51 +0545

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from userapp import views
+urlpatterns = [
+    path('register/',views.register,name='register.htm')
+    ]
